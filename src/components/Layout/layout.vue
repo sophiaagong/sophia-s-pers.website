1d6956c14f25430535ee85741b9b854e65df365a
sophiaagong <sophia.gong1207@gmail.com>
2025-06-27 16:03:06 +0800

    update

--- src/components/Layout/layout.vue
+++ src/components/Layout/layout.vue
@@ -2,9 +2,10 @@
 import {
     NConfigProvider,
 } from 'naive-ui';
+import HomeVue from '../home/home.vue';
 const themeOverrides = {
     common: {
-        "primaryColor": "#4350FFFF", // 这里可以自定义主题色
+        "primaryColor": "#E4D8FFFF", // 这里可以自定义主题色
         "primaryColorHover": "#747EFEFF",
         "primaryColorPressed": "#182084FF",
         "primaryColorSuppl": "#3B44BEFF",
@@ -15,11 +16,39 @@ const themeOverrides = {
 <template>
     <n-config-provider :theme-overrides="themeOverrides">
         <n-layout>
+
+            <n-layout-header>
+                <n-flex justify="'space-between'" :align="'center'">
+
+                    <n-flex class="logo" :align="'center'"> 
+                        <n-avatar round style="background: var(--primary color)">S</n-avatar>
+                        <div>Sophia's Website</div>
+                    </n-flex>
+                    <n-flex size="48">
+                        <n-button>ABOUT ME</n-button>
+                        <n-button>ACHIEVEMENTS</n-button>
+                        <n-button>CONTACTS</n-button>
+                        <n-button>CURRICULAR</n-button>
+                    </n-flex>
+                </n-flex>
+            </n-layout-header>
+
             <n-layout-content>
-                <router-view />
+                <home-vue></home-vue>
             </n-layout-content>
         </n-layout>
     </n-config-provider>
 </template>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.n-layout-header {
+    // 边距
+    padding: 2rem;
+}
+
+.logo {
+    font-size: 2rem;
+    font-weight: bold;
+    font-family: "Bebas Neue", sans-serif;
+}
+</style>
---
+++ src/components/home/home tutorial.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {NButton} from 'naive-ui';
+import {ref} from 'vue'
+function handleLightSwitch (){
+    light.value = !light.value;
+}
+const light = ref(false)
+</script>
+
+<template>
+    <!--  -->
+    <div style="color: pink">Hello, my name is Sophia!</div>
+    <div class="hello-container"></div>
+    <div class="paragraph-container" :style="{backgroundColor:Light ? 'yellow':'black'}"></div>
+        <n-button type="info"@click=handleLightSwitch>light switch</n-button>
+        paragraph-container
+    <h1>Hello, my name is Sophia!</h1>
+    <p>Hello, my name is Sophia</p>
+</template>
+
+<style scoped>
+.hello-container{
+    color:green
+}
+
+.paragraph-container{
+    background-color: white;
+    border: 2px solid black;
+}
+
+</style>
